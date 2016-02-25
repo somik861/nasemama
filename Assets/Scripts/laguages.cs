745243ff1cea2396d2ebb2b11386b7929ab76617
somik861 <janjuracka861@seznam.cz>
2016-02-25 15:47:03 +0100

    Přidáno pár slov do languages.cs

--- Assets/Scripts/laguages.cs
+++ Assets/Scripts/laguages.cs
@@ -5,11 +5,28 @@ public class laguages : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	string GetSlovo (string jazyk, string identifikator) {
+		if (jazyk == "cestina") {
+			switch (identifikator)
+			{
+			case "menu_startgame":
+				return "Nová hra";
+				break;
+			case "menu_loadgame":
+				return "Nahrát hru";
+				break;
+			case "menu_settings":
+				return "Nastavení";
+				break;
+			}
+		}
 	}
 }
