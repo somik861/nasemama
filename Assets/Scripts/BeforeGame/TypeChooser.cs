da20e5e9823b9f586382c308d8377575f5cd3dd0
somik861 <janjuracka861@seznam.cz>
2016-03-19 18:08:02 +0100

    Zpraven ID, opravena vada na scene In1v1Game (dosazeny skiny do skriptů)

--- Assets/Scripts/BeforeGame/TypeChooser.cs
+++ Assets/Scripts/BeforeGame/TypeChooser.cs
@@ -17,7 +17,7 @@ public class TypeChooser : MonoBehaviour {
     void OnGUI (){
         GUIStyle style = ChooserSkin.button;
         if (GUI.Button(new Rect(Screen.width/3, Screen.height / 8, Screen.width / 3, Screen.height / 1.25f), "", style)){
-            SceneManager.LoadScene("4");
+            SceneManager.LoadScene(4);
         }
     }
 }
--- Assets/Scripts/Menu/menu_script.cs
+++ Assets/Scripts/Menu/menu_script.cs
@@ -27,7 +27,7 @@ public class menu_script : MonoBehaviour {
         style.fontSize = (int)(Mathf.Round(Screen.width / 30));
             if (GUI.Button(new Rect(Screen.width / 200, Screen.height / 200, Screen.width / 2, Screen.height / 4.5f),language.GetSlovo(Jazyk, "menu_startgame"),style))
              {
-            SceneManager.LoadScene("3");
+            SceneManager.LoadScene(3);
              }
         if(GUI.Button(new Rect(Screen.width/4,Screen.height/4,Screen.width/2,Screen.height/4.5f),language.GetSlovo(Jazyk,"menu_loadgame"),style))
              {
@@ -36,7 +36,7 @@ public class menu_script : MonoBehaviour {
         if (GUI.Button(new Rect(Screen.width / 2, Screen.height / 2, Screen.width / 2, Screen.height / 4.5f), language.GetSlovo(Jazyk, "menu_settings"), style))
             
              {
-            SceneManager.LoadScene("1");
+            SceneManager.LoadScene(1);
              }
         GUIStyle style_vypinac = menuSkinVypinac.button;
         GUI.skin = menuSkinVypinac; 
